83fbc8d7e47f5a91651c670240cb7d60567fe7b6
Phani Karthik Maradani <karthik@mobikontech.com>
2016-02-06 15:01:58 +0530

    added mongo admin password

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/lotusvoice-dev'
+    uri: 'mongodb://admin:kiLyeSy8wlVe@localhost/lotusvoice-dev'
   },
 
   // Seed database on startup
